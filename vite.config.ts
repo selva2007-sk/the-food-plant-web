f663c4b057096bbb79bd8f79e1a81f2aab780e69
SELVAKUMAR S <selva2105sk@gmail.com>
2026-04-17 21:45:43 +0530

    update vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/the-food-plant-web/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
