54170402728bdd597f9b68a487b049b58d400948
SELVAKUMAR S <selva2105sk@gmail.com>
2026-04-17 21:49:38 +0530

    final update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/the-food-plant-web/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
